a5463b0883c2016fc203b634cf68efcc24f45ea7
alternateeffects <effectsalternate@gmail.com>
2025-07-12 18:15:53 +0200

    style update grid

--- generate_clips_json.ps1
+++ generate_clips_json.ps1
@@ -20,11 +20,20 @@ Get-ChildItem -Path $indir -Filter *.mp4 | ForEach-Object {
         if ($file.ToLower().Contains($key)) { $tags += $tagMap[$key] }
     }
     if ($tags.Count -eq 0) { $tags = @("other") }
+
+    # ---- ORIENTATION: rozpoznawanie za pomocą ffprobe ----
+    $ffout = & ffprobe -v error -select_streams v:0 -show_entries stream=width,height `
+                -of csv=s=x:p=0 "$indir\$file"
+    $w,$h = $ffout -split "x"
+    if (([int]$w) -ge ([int]$h)) { $orientation = "landscape" } else { $orientation = "portrait" }
+    # ---- END ORIENTATION ----
+
     $clipList += [PSCustomObject]@{
         title = $title
         file  = "$indir/$file"
         tags  = $tags
         thumbnail = ""
+        orientation = $orientation
     }
 }
 
