41c169e17daeea4cf3197c6d5ecf02ab8ebabd9d
alternateeffects <effectsalternate@gmail.com>
2025-07-19 20:49:35 +0200

    update

--- generate_thumbnails.bat
+++ generate_thumbnails.bat
@@ -1,15 +1,17 @@
 @echo off
-set input_folder=clips-watermarked
+set input_folder=clips
 set output_folder=thumbs-watermarked
 
 if not exist "%output_folder%" mkdir "%output_folder%"
 
-for %%F in (%input_folder%\*.mp4) do (
-    set name=%%~nF
+for /R "%input_folder%" %%F in (*.mp4) do (
+    set "name=%%~nF"
+    setlocal enabledelayedexpansion
     if not exist "%output_folder%\%%~nF.jpg" (
         echo Generuję miniaturkę do: %%~nxF
-        ffmpeg -y -ss 00:00:01.0 -i "%%F" -frames:v 1 -q:v 3 "%output_folder%\%%~nF.jpg"
+        ffmpeg -y -ss 00:00:03.0 -i "%%F" -frames:v 1 -q:v 3 "%output_folder%\%%~nF.jpg"
     )
+    endlocal
 )
 echo Gotowe!
 pause
